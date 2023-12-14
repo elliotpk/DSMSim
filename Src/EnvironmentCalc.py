62e975b3f8664d68c1b391dd3cfefb6ddf4c8cef
arvfroLTU <arvidfrom93@gmail.com>
2023-12-14 15:56:36 +0100

    whoops

--- Src/EnvironmentCalc.py
+++ Src/EnvironmentCalc.py
@@ -1,1 +1,29 @@
 import  ReferenceCalculator
+import googlemaps_test
+
+#6,458 Kilometres is distance between  rotterdam new york
+#
+#API_KEY = 'AIzaSyC8ObuqZq-i3Ppwu2SbxPez4K567ZTzQNk'
+#
+#origin = ( rand(mydatabase))
+#destination = mybidder([destination])
+#origin[continent] = "US"                               # continent can be either US or EU
+#destination[continent] =EU"
+
+#if origin(continent) == destination(continent)
+#       #calculate_distance_duration(API_KEY, origin, destination):  
+
+#else if continent(origin) == US
+#       #x = calculate_distance_duration(API_KEY, origin, New york):  
+        #y = sea route fuel
+        #z = #calculate_distance_duration(API_KEY, Rotterdam, destination):
+        
+# elseif contient(origin) == EU  
+#       #x = calculate_distance_duration(API_KEY, origin, Rotterdam):  
+        #y = sea route fuel
+        #z = #calculate_distance_duration(API_KEY, New york, destination): 
+        
+# else return "origin or destination not in allowed locations" 
+#
+#calculate_distance_duration(API_KEY, origin, destination):  
+#    
