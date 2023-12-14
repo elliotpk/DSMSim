6f4d995626b5b5ec25ed73a26a2a1d1e8e34c618
Svenss00nOskar <150705008+Svenss00nOskar@users.noreply.github.com>
2023-12-14 15:46:34 +0100

    Setting enviroment
    Added some comments in GUI.py explaining how to set up enviroment and the link to a guide showing it

--- Src/GUI.py
+++ Src/GUI.py
@@ -14,3 +14,8 @@ def hello():
 htmlLocation = os.path.dirname(os.path.realpath(__file__)) + "\\" + 'test.html'
 
 webbrowser.open_new_tab(htmlLocation)
+
+# Skriv i powershell:
+# DSMSimGrupp92023/.venv/Scripts/Activate.ps1
+# python -m flask --app .\Src\GUI.py run
+# Länk för guide: https://medium.com/@dipan.saha/managing-git-repositories-with-vscode-setting-up-a-virtual-environment-62980b9e8106
