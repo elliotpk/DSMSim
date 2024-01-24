0766a107624a830a7a212df550f84cc2ef78ebc2
danset-1 <danset-1@student.ltu.se>
2024-01-24 19:25:54 +0100

    Added map to result
    Backlog 3.3
    added a google map that can show routes

--- Src/GUI.py
+++ Src/GUI.py
@@ -21,14 +21,17 @@ b = y[1]
 def index():
     return render_template('index.html')
 
+
 companyNames = ['Company 1','Company 2','Company 3']
 @app.route('/result')
 def result():
     return render_template("result.html", names = companyNames, data = a, data2 = b)
 
+
 @app.route('/config')
 def config():
     return render_template("config.html")
+
 
 htmlLocation = 'http://127.0.0.1:5000'
 webbrowser.open_new_tab(htmlLocation)
--- Src/static/script/script.js
+++ Src/static/script/script.js
@@ -1,3 +1,4 @@
+
 function changeClass0(){
     var element = document.querySelector("#nr0");
     element.classList.replace("options", "selected");
@@ -50,3 +51,41 @@ function changeClass2(){
     
 }
 
+//start coordinates for the map //its coordinates for stockholm
+var start = { lat: 59.326038, lng:17.8172531};
+
+let directionsService, directionsRenderer
+
+//initialize the Google Map
+function initMap(){
+    
+var mapOptions = {
+    center: start,
+    zoom: 4,
+};
+
+map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
+
+directionsService = new google.maps.DirectionsService()
+directionsRenderer = new google.maps.DirectionsRenderer()
+directionsRenderer.setMap(map)
+
+calcRoute();
+}
+
+//Make a route and then display it on the map
+function calcRoute(){
+  var source ="Linköping";
+  var destination = "Stockholm";
+
+  let req = {
+    origin: source,
+    destination: destination,
+    travelMode: "DRIVING",
+  };
+  directionsService.route(req,function(result,status){
+    if(status == "OK"){
+        directionsRenderer.setDirections(result)
+    }
+  })
+}
