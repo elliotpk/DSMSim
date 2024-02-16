7567f4ee593ee73301b550a6a30e70bfb559601b
Arvid From/home/arvfro4/.venv/bin/python <arvidfrom93@gmail.com>
2024-02-16 12:41:28 +0100

    handled edgeCases in buildNet

--- Src/buildNet.py
+++ Src/buildNet.py
@@ -34,12 +34,19 @@ for loop that identifies each specific country and runs the following on said co
 * How to identify best city to neighbouring country
 '''
 
-
+lastconnection = "connection4"                      #if more connections  than 4 are desired, the maximum amount of connections should be inserted here
 
 def buildNet():
+    "collects Country networks and connects them"
     return
+
+def cityBuilder(name):
+    "Creates a city object"
+    x= placeClasses.City(name)
+    return x
     
 def countryBuilder():
+    "builds a country object that contains relevant city objects, given a structured csv file"
     countriesWithCities =[]
     built = []
     with open('Database/places.csv', 'r', newline='', encoding='utf-8') as csvfile:
@@ -55,43 +62,48 @@ def countryBuilder():
                     if row[1] != countryName:       
                         break
                     else:
-                        countryObject.cities.append(row[0])
+                        countryObject.cities.append(cityBuilder(row[0]))    # appends city objects
  
                 built.append(x)
                 countriesWithCities.append(countryObject) 
             count += 1
     return countriesWithCities 
-
-def cityBuilder(name):
-    x= placeClasses.City(name)
-    return x
+    
+ def connectionsScanner(cityObj):
+    "returns which delivery-connection should be established"
+    x= cityObj.connections
+    count= 1
+    for i in range(len(x)):
+        if (i ==None):
+            return count
+        else: count += 1
+    return -1   
     
            
 def countryNet(countryObj):
+    "takes a single country object and establishes a proximity net which is represented by a list of arrays"
     
     cities = countryObj.cities
-    citiesObj = []
     countryNet = []
     
-    for i in range(0, len(cities)): 
-        scan=cityBuilder(cities[i])  # converts cities from variable to object   
-        citiesObj.append(scan)
 
-    for i in range(0, len(citiesObj)):
+    for i in range(0, len(cities)):
         distanceList = []
         
-        for j in range(0, len(citiesObj)):
+        for j in range(0, len(cities)):
             
-            distance = API_Handling.Route(API_KEY,citiesObj[i],citiesObj[j])
-            distanceList.append([citiesObj[j], distance])
-            
-            
-        '''put smallest nonzero value  in connection 1 , 2nd smallest connection2 etc.'''
-        '''### PROBLEM   when a city searches, it should exclude itself, this does not currently happen  PROBLEM ###'''
+            distance = API_Handling.Route(API_KEY,cities[i],cities[j])
+            distanceList.append([cities[j].name, distance])
         
+    
         sorted= sorted(distanceList)                #returns sorted range of distances from other cities
         
-        for j in range(0, len(sorted)):                                  
+        '''###PROBLEM  does not ensure that all cities have connections currently PROBLEM ###'''
+        
+        ''' if closest city is full of connections, establish a connection with their least significant connection, erase previous connection'''
+        
+      
+        for j in range(1, len(sorted)):                                              #the 1 excludes the closest city (itself)
             
             openConnection1 = connectionsScanner(sorted[i])                           #checks for open connection 
             openConnection2 = connectionsScanner(sorted[j])
@@ -104,33 +116,25 @@ def countryNet(countryObj):
                     setattr(sorted[i], name1, sorted[j] )                             # sets  an open connection in city1 to (city2, distance)
                     setattr(sorted[j], name2, ([sorted[i][0], sorted[j][1]]))         # sets connection  in city2 to [city1, distance]
 
-                    
                     list1 = getattr(sorted[i, "connections"])
                     list1[openConnection1] = 1                                        # sets closed flag on object 1 connections
                     setattr(sorted[i], "connections", list1)
                     
-                    list2 = getattr([sorted[j], "connections"])                        # # sets closed flag on object 2 connections                    
+                    list2 = getattr([sorted[j], "connections"])                       # # sets closed flag on object 2 connections                    
                     list2[openConnection2] = 1              
                     setattr(sorted[i], "connections", list2 )
-                    
+            
+            
+            elif(openConnection2 == -1):                # if the closest city is full of connections
+                
+                if (getattr(sorted[i], name1) == None):
+                    setattr(sorted[i], name1, sorted[j])
+                    setattr(sorted[j], lastconnection, ([sorted[i][0], API_Handling.Route(sorted[j].name,sorted[i].name)]))   # replace last connection on city2, so that every city is in network
+                    break           # this should only happen to one city   
+
         countryNet.append(sorted[i])
         
     return countryNet
-            
-                    
-        
-                
-                
-            
-def connectionsScanner(cityObj):
-    "returns which connection should be established"
-    x= cityObj.connections
-    count= 1
-    for i in range(len(x)):
-        if (i ==None):
-            return count
-        else: count += 1
-    return -1
 
     
     
