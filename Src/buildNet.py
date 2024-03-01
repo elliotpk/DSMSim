908a7825eb9d39bd76e5239d98639f8a0e5e53ae
Clevermazter <oliver@ostrot.se>
2024-03-01 14:26:28 +0100

    Fixed Try Except

--- Src/buildNet.py
+++ Src/buildNet.py
@@ -40,8 +40,9 @@ def allNationalNetworks():
 
     for i in range(1, len(countriesWithCities)): 
         country =countriesWithCities[i].name
-        cities = countryNet(countriesWithCities[i])
-
+        
+        cities = countryNet(countriesWithCities[i])             #"Problems"
+        
         nationalnetworks[country].append(cities) 
     return nationalnetworks
 
@@ -108,14 +109,17 @@ def countryNet(countryObj):
         distanceList = []
         for j in range(0, len(cities)):
             distance = API_Handling.Route2(cities[i].name,cities[j].name)
-            try:
-                sortedx= sorted(distanceList)
-            except:
-                sortedx = distanceList
+            distanceList.append([distance, cities[j]]) 
+        try:    
+            sortedx= sorted(distanceList)
+        except:
+            sortedx = distanceList   
+                     
+        for j in range(0, len(sortedx)):
             x =insertion_sort(sortedx)
         
         
-        "This segment prepares everything for the output dictionary the three closest cities are regarded for the network"
+        "This segment prepares everything for the output dictionary. it establishes the three closest cities egarded for the network"
         z= []
         zObj= []
         
